d60fca40b1cd6bceb7eefb11f5949c174f754159
nevadskiy <nevadskiy@gmail.com>
2023-09-14 22:21:17 +0300

    wip

--- src/GetResults.php
+++ src/GetResults.php
@@ -11,44 +11,40 @@ use Illuminate\Database\Eloquent\Relations\MorphPivot;
  */
 trait GetResults
 {
-	/**
-	 * Get the results of the relationship.
-	 */
+	protected $morphDictionaries = [];
+
 	public function getResults(): Collection
 	{
 		if ($this->parent->getAttribute($this->parentKeyName) === null) {
 			return $this->newCollection();
 		}
 
+		// * Query pivot table.
+		// * Collect all morph type and keys from pivot (use single loop).
+		// * For each morph type, generate a query that
+		// * Generate separate queries for each morph type using whereIn method.
+		// * Find morph model for each pivot and put to result.
 
-		/** @var Collection<MorphPivot> $pivotModels */
 		$pivotModels = $this->query->get();
 
-		// @todo hydrate pivot model here...
-
-		$keysByMorphType = $this->gatherKeysByMorphType($pivotModels);
-
-		$morphDictionaries = [];
-
-		foreach ($keysByMorphType as $morphType => $morphKeys) {
-			$morphDictionaries[$morphType] = $this->getResultsByMorphType($morphType, array_keys($morphKeys))->getDictionary();
-		}
+		$this->buildDictionary($pivotModels);
 
 		$results = [];
 
 		foreach ($pivotModels as $pivotModel) {
-			$morphType = $this->getDictionaryKey($pivotModel->getAttribute($this->pivotMorphTypeName));
-			$morphForeignKey = $this->getDictionaryKey($pivotModel->getAttribute($this->pivotMorphForeignKeyName));
-
-			$result = $morphDictionaries[$morphType][$morphForeignKey]; // @todo handle missing model...
-
-			// @todo hydrate pivot at the beginning of the method
-			$this->hydratePivotModel($result, $pivotModel);
-
-			$results[] = $result;
+			$results[] = $this->getResultForPivot($pivotModel);
 		}
 
 		return $this->newCollection($results);
+	}
+
+	protected function buildDictionary(Collection $pivotModels): void
+	{
+		$keysByMorphType = $this->gatherKeysByMorphType($pivotModels);
+
+		foreach ($keysByMorphType as $morphType => $morphKeys) {
+			$this->morphDictionaries[$morphType] = $this->getResultsByMorphType($morphType, $morphKeys)->getDictionary();
+		}
 	}
 
 	/**
@@ -56,19 +52,22 @@ trait GetResults
 	 */
 	protected function gatherKeysByMorphType(Collection $pivotModels): array
 	{
-		$morphKeys = [];
+		$keysByMorphType = [];
 
-		// @todo hydrate keys to Pivot model... Pivot::setMorphs() to not build dictionary key again later.
 		foreach ($pivotModels as $pivotModel) {
 			if ($pivotModel->getAttribute($this->pivotMorphTypeName)) {
 				$morphTypeKey = $this->getDictionaryKey($pivotModel->getAttribute($this->pivotMorphTypeName));
 				$morphForeignKey = $this->getDictionaryKey($pivotModel->getAttribute($this->pivotMorphForeignKeyName));
 
-				$morphKeys[$morphTypeKey][$morphForeignKey] = true;
+				$keysByMorphType[$morphTypeKey][$morphForeignKey] = true;
 			}
 		}
 
-		return $morphKeys;
+		foreach ($keysByMorphType as $morphType => $morphKeys) {
+			$keysByMorphType[$morphType] = array_unique($morphKeys);
+		}
+
+		return $keysByMorphType;
 	}
 
 	/**
@@ -120,5 +119,18 @@ trait GetResults
 	protected function newCollection(array $models = []): Collection
 	{
 		return new Collection($models);
+	}
+
+	protected function getResultForPivot(MorphPivot $pivotModel): mixed
+	{
+		$morphType = $this->getDictionaryKey($pivotModel->getAttribute($this->pivotMorphTypeName));
+		$morphForeignKey = $this->getDictionaryKey($pivotModel->getAttribute($this->pivotMorphForeignKeyName));
+
+		$result = $this->morphDictionaries[$morphType][$morphForeignKey]; // @todo handle missing model...
+
+		// @todo hydrate pivot at the beginning of the method
+		$this->hydratePivotModel($result, $pivotModel);
+
+		return $result;
 	}
 }
